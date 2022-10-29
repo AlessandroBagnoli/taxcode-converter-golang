2ba931d7bfcdae7b54746b93eab372618f911f48
Alessandro Bagnoli <alessandro.bagnoli4@gmail.com>
2022-10-29 16:12:08 +0200

    feat: added req/res objects for calculate tax code

--- service/objects.go
+++ service/objects.go
@@ -2,17 +2,22 @@ package service
 
 import "time"
 
-type CalculatePersonDataRequest struct {
+type CalculateTaxCodeRequest struct {
+	Gender      Gender    `json:"gender"`
+	Name        string    `json:"name"`
+	Surname     string    `json:"surname"`
+	DateOfBirth time.Time `json:"dateOfBirth"`
+	BirthPlace  string    `json:"birthPlace"`
+	Province    string    `json:"province"`
+}
+
+type CalculateTaxCodeResponse struct {
 	TaxCode string `json:"taxCode"`
 }
 
-type Gender string
-
-const (
-	GenderUnknown Gender = "UNKNOWN"
-	GenderMale           = "M"
-	GenderFemale         = "F"
-)
+type CalculatePersonDataRequest struct {
+	TaxCode string `json:"taxCode"`
+}
 
 type CalculatePersonDataResponse struct {
 	Gender      Gender    `json:"gender"`
@@ -23,3 +28,11 @@ type CalculatePersonDataResponse struct {
 	Province    string    `json:"province"`
 	TaxCode     string    `json:"taxCode"`
 }
+
+type Gender string
+
+const (
+	GenderUnknown Gender = "UNKNOWN"
+	GenderMale           = "M"
+	GenderFemale         = "F"
+)
