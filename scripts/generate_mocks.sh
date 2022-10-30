1a80ad29d463f0bf5d44f866cf68080bb561d620
Alessandro Bagnoli <alessandro.bagnoli4@gmail.com>
2022-10-30 17:47:30 +0100

    chore: unified code generation

--- scripts/generate_mocks.sh
+++
@@ -1,2 +0,0 @@
-cd .. || exit
-docker run --rm -v "$PWD":/src -w /src vektra/mockery --all --output ./service/mocks
--- scripts/generate_swagger.sh
+++
@@ -1,3 +0,0 @@
-cd .. || exit
-go install github.com/swaggo/swag/cmd/swag@latest
-swag init --pd
