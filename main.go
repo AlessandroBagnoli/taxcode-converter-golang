88807155f58c63a4b747aab5322b293a1b022563
Alessandro Bagnoli <alessandro.bagnoli4@gmail.com>
2022-10-30 19:18:39 +0100

    refactor: setup file for reusing of functions used for bootstrapping the app

--- main.go
+++ main.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"cloud.google.com/go/civil"
-	"github.com/go-playground/validator/v10"
-	"github.com/go-playground/validator/v10/non-standard/validators"
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	swaggerfiber "github.com/gofiber/swagger"
 	log "github.com/sirupsen/logrus"
-	"strings"
 	_ "taxcode-converter/docs"
 	"taxcode-converter/service/handler"
-	"taxcode-converter/service/taxcode"
-	validatorservice "taxcode-converter/service/validation"
 )
 
 // @title taxcode-converter
@@ -23,62 +13,7 @@ import (
 //
 //go:generate swag init --pd
 func main() {
-
-	h := initDependencies()
-
-	app := createFiberApp(h)
-
-	// server listening
+	h := handler.InitDependencies()
+	app := handler.CreateFiberApp(h)
 	log.Fatal(app.Listen(":8080"))
-}
-
-// initDependencies creates and injects dependencies, returns the handler for incoming http requests
-func initDependencies() handler.Handler {
-	taxCodeService := taxcode.NewTaxCodeService()
-	validate := configureValidator()
-	v := validatorservice.NewValidator(validate)
-	h := handler.NewHandler(taxCodeService, v)
-	return h
-}
-
-// createFiberApp creates and configure the fiber server
-func createFiberApp(h handler.Handler) *fiber.App {
-	// creation of fiber app with custom config for error handling, usage of logger and cors
-	app := fiber.New(fiber.Config{ErrorHandler: h.HandleError})
-	app.Use(configureFiberLogger())
-	app.Use(cors.New())
-
-	// routing for swagger documentation
-	app.Get("/swagger/*", swaggerfiber.HandlerDefault)
-
-	// routing for apis
-	v1 := app.Group("/api/v1")
-	v1.Post("/taxcode:calculate-tax-code", h.CalculateTaxCode)
-	v1.Post("/taxcode:calculate-person-data", h.CalculatePersonData)
-	return app
-}
-
-func configureFiberLogger() fiber.Handler {
-	return logger.New(logger.Config{
-		// this is because I just want to log incoming request to the exposed api and ignore everything else
-		Next: func(c *fiber.Ctx) bool {
-			return !strings.Contains(c.Path(), "/api/v1/")
-		},
-		Format: "[${time}]|${status}|${resBody}|${latency} - ${method}|${path}|${body}\n",
-	})
-}
-
-func configureValidator() validator.Validate {
-	validate := validator.New()
-	if err := validate.RegisterValidation("notblank", validators.NotBlank); err != nil {
-		log.Fatal(err)
-	}
-	if err := validate.RegisterValidation("taxcode", validatorservice.ValidTaxCode); err != nil {
-		log.Fatal(err)
-	}
-	validate.RegisterCustomTypeFunc(validatorservice.TimeValue, civil.Date{}, civil.DateTime{}, civil.Time{})
-	if err := validate.RegisterValidation("inthepast", validatorservice.DateInThePast); err != nil {
-		log.Fatal(err)
-	}
-	return *validate
 }
--- service/handler/handler_test.go
+++ service/handler/handler_test.go
@@ -4,72 +4,14 @@ import (
 	"bytes"
 	"cloud.google.com/go/civil"
 	"encoding/json"
-	"github.com/go-playground/validator/v10"
-	"github.com/go-playground/validator/v10/non-standard/validators"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	swaggerfiber "github.com/gofiber/swagger"
 	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"io"
 	"net/http/httptest"
-	"strings"
 	"taxcode-converter/service"
-	"taxcode-converter/service/taxcode"
-	validatorservice "taxcode-converter/service/validation"
 	"testing"
 )
-
-// TODO all these setup methods are just copied from main.go. Find a place where to put them in order to reuse them also in test without copying everytime
-func setup() Handler {
-	taxCodeService := taxcode.NewTaxCodeService()
-	validate := configureValidator()
-	v := validatorservice.NewValidator(validate)
-	h := NewHandler(taxCodeService, v)
-	return h
-}
-
-func configureValidator() validator.Validate {
-	validate := validator.New()
-	if err := validate.RegisterValidation("notblank", validators.NotBlank); err != nil {
-		log.Fatal(err)
-	}
-	if err := validate.RegisterValidation("taxcode", validatorservice.ValidTaxCode); err != nil {
-		log.Fatal(err)
-	}
-	validate.RegisterCustomTypeFunc(validatorservice.TimeValue, civil.Date{}, civil.DateTime{}, civil.Time{})
-	if err := validate.RegisterValidation("inthepast", validatorservice.DateInThePast); err != nil {
-		log.Fatal(err)
-	}
-	return *validate
-}
-
-func createFiberApp(h Handler) *fiber.App {
-	// creation of fiber app with custom config for error handling, usage of logger and cors
-	app := fiber.New(fiber.Config{ErrorHandler: h.HandleError})
-	app.Use(configureFiberLogger())
-	app.Use(cors.New())
-
-	// routing for swagger documentation
-	app.Get("/swagger/*", swaggerfiber.HandlerDefault)
-
-	// routing for apis
-	v1 := app.Group("/api/v1")
-	v1.Post("/taxcode:calculate-tax-code", h.CalculateTaxCode)
-	v1.Post("/taxcode:calculate-person-data", h.CalculatePersonData)
-	return app
-}
-
-func configureFiberLogger() fiber.Handler {
-	return logger.New(logger.Config{
-		// this is because I just want to log incoming request to the exposed api and ignore everything else
-		Next: func(c *fiber.Ctx) bool {
-			return !strings.Contains(c.Path(), "/api/v1/")
-		},
-		Format: "[${time}]|${status}|${resBody}|${latency} - ${method}|${path}|${body}\n",
-	})
-}
 
 func TestHandler_CalculateTaxCode(t *testing.T) {
 	tests := []struct {
@@ -120,8 +62,8 @@ func TestHandler_CalculateTaxCode(t *testing.T) {
 	for _, test := range tests {
 		t.Run(test.description, func(t *testing.T) {
 			// given
-			h := setup()
-			app := createFiberApp(h)
+			h := InitDependencies()
+			app := CreateFiberApp(h)
 			marshal, _ := json.Marshal(test.body)
 			req := httptest.NewRequest(fiber.MethodPost, test.route, bytes.NewReader(marshal))
 			req.Header.Set(fiber.HeaderContentType, fiber.MIMEApplicationJSON)
