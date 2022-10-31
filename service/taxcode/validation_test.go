9b41d75684b2e201fdda73daade2a4c3e6fb5e50
Alessandro Bagnoli <alessandro.bagnoli4@gmail.com>
2022-10-31 23:35:27 +0100

    test: testing

--- service/taxcode/validation_test.go
+++ service/taxcode/validation_test.go
@@ -14,7 +14,7 @@ func setup() validator.Validate {
 	return CreateValidator()
 }
 
-func TestValidateReq(t *testing.T) {
+func TestValidateCalculateTaxCodeRequest(t *testing.T) {
 	v := setup()
 
 	tests := []struct {
@@ -37,6 +37,55 @@ func TestValidateReq(t *testing.T) {
 				Province:   "RM",
 			},
 			expected: nil,
+		},
+		{
+			description: "should return error when CalculateTaxCodeRequest is not ok",
+			input: service.CalculateTaxCodeRequest{
+				Gender:  "MALE",
+				Name:    " ",
+				Surname: "Rossi",
+				DateOfBirth: civil.Date{
+					Year:  2000,
+					Month: 1,
+					Day:   1,
+				},
+				BirthPlace: "Roma",
+				Province:   "RM",
+			},
+			expected: service.NewRuntimeError(400, "name must not be blank"),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			// given
+
+			// when
+			actual := ValidateReq(v, test.input)
+
+			// then
+			assert.Equal(t, test.expected, actual)
+		})
+	}
+}
+
+func TestValidateCalculatePersonDataRequest(t *testing.T) {
+	v := setup()
+
+	tests := []struct {
+		description string
+		input       service.CalculatePersonDataRequest
+		expected    error
+	}{
+		{
+			description: "should return nil when CalculatePersonDataRequest is ok",
+			input:       service.CalculatePersonDataRequest{TaxCode: "TSMFZF92H15F081V"},
+			expected:    nil,
+		},
+		{
+			description: "should return error when CalculateTaxCodeRequest is not ok",
+			input:       service.CalculatePersonDataRequest{TaxCode: "dummyTaxCode"},
+			expected:    service.NewRuntimeError(400, "taxCode must be a valid tax code"),
 		},
 	}
 
